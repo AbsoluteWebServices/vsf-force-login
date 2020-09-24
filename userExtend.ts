c0afe5ebb21beb94ea6fda87d6c6d8d7f0260ffa
Dmitry Kozachek <dmitry.kozachek@gmail.com>
2020-09-24 21:03:43 +0300

    feat: Use core startSession instead of copy

--- userExtend.ts
+++ userExtend.ts
@@ -1,8 +1,5 @@
 import Vue from 'vue'
-import config from 'config'
-import { isServer } from '@vue-storefront/core/helpers'
-import { Logger } from '@vue-storefront/core/lib/logger'
-import * as types from '@vue-storefront/core/modules/user/store/mutation-types'
+import coreActions from '@vue-storefront/core/modules/user/store/actions'
 
 const setTokens = async () => {
   const params = new URLSearchParams(location.search)
@@ -19,53 +16,12 @@ const setTokens = async () => {
   await cache.setItem('current-refresh-token', refreshToken)
 }
 
-const coreStartSession = async (context) => {
-  if (isServer || context.getters.isLocalDataLoaded) return
-  const cache = Vue.prototype.$db.usersCollection
-
-  const user = await cache.getItem(`current-user`)
-
-  if (user) {
-    context.commit(types.USER_INFO_LOADED, user)
-  }
-
-  context.commit(types.USER_START_SESSION)
-  context.commit(types.USER_LOCAL_DATA_LOADED, true)
-
-  cache.getItem('current-token', (err, res) => {
-    if (err) {
-      Logger.error(err, 'user')()
-      return
-    }
-
-    if (res) {
-      context.commit(types.USER_TOKEN_CHANGED, { newToken: res })
-      context.dispatch('sessionAfterAuthorized')
-
-      if (config.usePriceTiers) {
-        cache.getItem('current-user', (err, userData) => {
-          if (err) {
-            Logger.error(err, 'user')()
-            return
-          }
-
-          if (userData) {
-            context.dispatch('setUserGroup', userData)
-          }
-        })
-      }
-    } else {
-      Vue.prototype.$bus.$emit('session-after-nonauthorized')
-    }
-    Vue.prototype.$bus.$emit('session-after-started')
-  })
-}
-
 const extendUserVuex = {
   actions: {
-    async startSession (context) {
+    coreStartSession: coreActions.startSession,
+    async startSession ({ dispatch }) {
       await setTokens()
-      return coreStartSession(context)
+      return dispatch('coreStartSession')
     }
   }
 }
