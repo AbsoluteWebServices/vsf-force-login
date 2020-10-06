e8ff4b599fa4e6c75db2de3ea571b4cdd6414df1
Brian McDonald <bmcdonald@luxbp.com>
2020-10-06 11:31:05 -0400

    feat(cart-clear): clear existing cart from cache

--- userExtend.ts
+++ userExtend.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import coreActions from '@vue-storefront/core/modules/user/store/actions'
+import rootStore from '@vue-storefront/core/store'
 
 const setTokens = async () => {
   const params = new URLSearchParams(location.search)
@@ -8,15 +9,26 @@ const setTokens = async () => {
     return
   }
 
-  const token = params.get('__sso')
-  const cache = Vue.prototype.$db.usersCollection
+  rootStore.dispatch('user/clearCurrentUser', {silent: true}).then(() => {
+    // clear cart cache
+    const cartCache = Vue.prototype.$db.cartsCollection
+    cartCache.setItem('current-cart-token', null)
+    cartCache.setItem('current-cart-hash', null)
+    cartCache.setItem('current-cart', null)
 
-  await cache.setItem('current-token', token)
+    const token = params.get('__sso')
+    const cache = Vue.prototype.$db.usersCollection
 
-  if (params.hasOwnProperty('__sst')) {
-    const refreshToken = params.get('__sst')
-    await cache.setItem('current-refresh-token', refreshToken)
-  }
+    cache.setItem('current-token', token)
+
+    // we add to the cache we came from sso, useful for detecting on auth page
+    cache.setItem('source-sso', true)
+
+    if (params.hasOwnProperty('__sst')) {
+      const refreshToken = params.get('__sst')
+      cache.setItem('current-refresh-token', refreshToken)
+    }
+  })
 }
 
 const extendUserVuex = {
